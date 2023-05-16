521762f59dcdeaa85128fde076f736cdaedb103a
yeongseo1204 <dys951204@gmail.com>
2023-05-16 14:00:36 +0900

    해더랑 메인 화면 검색창 수정

---
+++ src/main/java/com/study/bookspace/member/MemberController.java
@@ -0,0 +1,21 @@
+package com.study.bookspace.member;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/member")
+public class MemberController {
+	
+	@GetMapping("/joinForm")
+	public String joinForm() {
+		return "content/member/join";
+	}
+	
+	@GetMapping("/loginForm")
+	public String loginForm() {
+		return "content/member/login";
+	}
+
+}
