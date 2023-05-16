5455f232581c2c8187597cfba1dc16af53e2e778
huhjs <xxxyzzzky@gmail.com>
2023-05-16 16:22:10 +0900

    패키지 수정

--- src/main/java/com/study/bookspace/club/controller/ClubController.java
+++ src/main/java/com/study/bookspace/club/controller/ClubController.java
@@ -1,4 +1,4 @@
-package com.study.bookspace.club;
+package com.study.bookspace.club.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
--- src/main/java/com/study/bookspace/member/controller/MemberController.java
+++ src/main/java/com/study/bookspace/member/controller/MemberController.java
@@ -1,4 +1,4 @@
-package com.study.bookspace.member;
+package com.study.bookspace.member.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
