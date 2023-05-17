9d18af6a1c361c819c97fc80792e2b829f16e313
soyeong111 <sssssoyeeeong@gmail.com>
2023-05-17 17:32:19 +0900

    ddd

--- src/main/java/com/study/bookspace/info/service/QnaService.java
+++ src/main/java/com/study/bookspace/info/service/QnaService.java
@@ -1,5 +1,8 @@
 package com.study.bookspace.info.service;
 
 public interface QnaService {
+	
+	//질문 작성
+	void insertQna();
 
 }
--- src/main/java/com/study/bookspace/info/service/QnaServiceImpl.java
+++ src/main/java/com/study/bookspace/info/service/QnaServiceImpl.java
@@ -1,5 +1,19 @@
 package com.study.bookspace.info.service;
 
-public class QnaServiceImpl {
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service("qnaService")
+public class QnaServiceImpl implements QnaService {
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+
+	//질문 작성
+	@Override
+	public void insertQna() {
+		sqlSession.insert("qnaMpper.insertQna");
+		
+	}
 
 }
