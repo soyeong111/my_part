259669d0166ca3a47a6278ac54575ffd16fec076
huhjs <xxxyzzzky@gmail.com>
2023-05-18 14:11:50 +0900

    수정

--- src/main/java/com/study/bookspace/book/service/BookServiceImpl.java
+++ src/main/java/com/study/bookspace/book/service/BookServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.study.bookspace.book.vo.BookVO;
 import com.study.bookspace.book.vo.CategoryVO;
@@ -23,6 +24,18 @@ public class BookServiceImpl implements BookService {
 	@Override
 	public List<BookVO> getBookListForUser() {
 		return sqlSession.selectList("bookMapper.getBookListForUser");
+	}
+
+	@Override
+	public String getNextBookCode() {
+		return sqlSession.selectOne("bookMapper.getNextBookCode");
+	}
+
+	@Override
+	@Transactional(rollbackFor =  Exception.class)
+	public void regBook(BookVO bookVO) {
+		sqlSession.insert("bookMapper.regBook", bookVO);
+		sqlSession.insert("bookMapper.insertImges", bookVO);
 	}
 	
 	
--- src/main/java/com/study/bookspace/book/vo/BookVO.java
+++ src/main/java/com/study/bookspace/book/vo/BookVO.java
@@ -1,5 +1,8 @@
 package com.study.bookspace.book.vo;
 
+import java.util.List;
+
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -18,5 +21,7 @@ public class BookVO {
 	private String bookCateNo;
 	private String bookStockCnt;
 	private String isbn;
+	private List<ImgVO> imgList; // 상품 1개는 이미지 정보(ImgVO) 여러개를 갖고 있다
+	private CategoryVO categoryVO; // 아이템정보 1개는 카테고리 정보를 갖고 있다
 }
 
