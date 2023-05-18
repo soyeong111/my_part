93926f257fa5c25a00d3b32d80e73a9cad9beb88
sue0336 <suesue0336@gmail.com>
2023-05-18 12:14:00 +0900

    util2

--- src/main/java/com/study/bookspace/util/ConstVariable.java
+++ src/main/java/com/study/bookspace/util/ConstVariable.java
@@ -3,11 +3,11 @@ package com.study.bookspace.util;
 public class ConstVariable {
 	
 	// 도서 이미지 첨부 파일 경로
-    public static final String BOOK_UPLOAD_PATH = "D:\\dev\\workspaceSTS\\BookSpace\\src\\main\\resources\\static\\images\\book\\";
+    public static final String BOOK_UPLOAD_PATH = "D:\\dev\\workspaceSTS\\BookSpace\\src\\main\\resources\\static\\image\\book\\";
 
     
     // 클럽 이미지 첨부 파일 경로
-    public static final String CLUB_UPLOAD_PATH = "D:\\dev\\workspaceSTS\\BookSpace\\src\\main\\resources\\static\\images\\club\\";
+    public static final String CLUB_UPLOAD_PATH = "D:\\dev\\workspaceSTS\\BookSpace\\src\\main\\resources\\static\\image\\club\\";
 
 	
 	// ASCII 범위 – 영숫자(0-9, a-z, A-Z)
