632fe1b0825229860759cdff06c96371a8944205
soyeong111 <sssssoyeeeong@gmail.com>
2023-05-22 08:35:51 +0900

    hi

--- src/main/java/com/study/bookspace/info/vo/AnswerVO.java
+++ src/main/java/com/study/bookspace/info/vo/AnswerVO.java
@@ -1,5 +1,17 @@
 package com.study.bookspace.info.vo;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
 public class AnswerVO {
+	private String answerCode;
+	private String answerContent;
+	private String answerCreateDate;
+	private String memId;
+	private String qnaCode;
 
 }
