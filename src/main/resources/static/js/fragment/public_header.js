8a0e3c91638b7dfb323fc672e2df372985db33fa
yeongseo1204 <dys951204@gmail.com>
2023-05-23 01:34:14 +0900

    ys05230134

--- src/main/resources/static/js/fragment/public_header.js
+++ src/main/resources/static/js/fragment/public_header.js
@@ -6,3 +6,13 @@ function fixed_menu_mouseenter() {
 function fixed_menu_mouseleave() {
 	document.querySelector('.header-unfixed-div').classList.add('d-none');
 }
+
+function main_menu_mouseover(index) {
+	const menus = document.querySelectorAll('.header-sub-menu-div > div');
+	menus[index].classList.add('main-sub-menu-hover');
+}
+
+function main_menu_mouseout(index) {
+	const menus = document.querySelectorAll('.header-sub-menu-div > div');
+	menus[index].classList.remove('main-sub-menu-hover');
+}
