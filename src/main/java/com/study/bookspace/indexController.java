81270fe722d2169c9380b36dd5abd3675dc3e9ea
yeongseo1204 <dys951204@gmail.com>
2023-05-22 16:04:41 +0900

    ys05221604

--- src/main/java/com/study/bookspace/indexController.java
+++ src/main/java/com/study/bookspace/indexController.java
@@ -3,11 +3,13 @@ package com.study.bookspace;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.study.bookspace.admin.vo.SubMenuVO;
+
 @Controller
 public class indexController {
 	
 	@GetMapping("/")
-	public String index() {
+	public String index(SubMenuVO subMenuVO) {
 		return "content/main";
 	}
 
---
+++ src/main/resources/static/js/fragment/public_header.js
@@ -0,0 +1,8 @@
+
+function menu_mouseenter() {
+	document.querySelector('.header-sub-menu-div').classList.remove('d-none');
+}
+
+function menu_mouseleave() {
+	document.querySelector('.header-sub-menu-div').classList.add('d-none');
+}
