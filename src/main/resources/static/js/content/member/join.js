7d265ebfeb2b99f55dd421565f7ce3631361a6a5
yeongseo1204 <dys951204@gmail.com>
2023-05-31 12:05:45 +0900

    ys05311205

--- src/main/resources/static/js/content/member/join.js
+++ src/main/resources/static/js/content/member/join.js
@@ -1,5 +1,34 @@
 
-let join_email_pw = '';
+const join_email_obj = {}
+const join_tell_obj = {}
+
+/* 타이머 시작 */
+function startTimer(obj) {
+	obj.minute = 5;
+	obj.second = 0;
+	obj.timer = setInterval(countTimer, 1000, obj);
+}
+
+/* 1초 마다 실행 */
+function countTimer(obj) {
+	obj.div.textContent = `${obj.minute.toString().padStart(2, '0')}:${obj.second.toString().padStart(2, '0')}`;
+	if (obj.second == 0) {
+		if (obj.minute > 0) {
+			obj.minute--;
+			obj.second = 59;
+		} else {
+			clearInterval(obj.timer);
+			const p_div = obj.div.parentElement;
+			const s_div = p_div.nextElementSibling;
+			p_div.remove();
+			if (s_div.firstElementChild.classList.contains('invalid-text-div')) {
+				s_div.remove();
+			}
+		}
+	} else {
+		obj.second--;
+	}
+}
 
 /* 아이디 유효성 검사 */
 function id_validation(id_input) {
@@ -43,6 +72,7 @@ function id_duplicate_check(btn) {
 				invalid_text_div.remove();
 			} else {
 				invalid_text_div.textContent = '*이미 사용중인 아이디입니다.';
+				id_input.focus();
 			}
 		},
 		error: function() {
@@ -181,12 +211,15 @@ function email_pw_send(btn) {
 		contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
 		data: {'email':email},
 		success: function(result) {
-			join_email_pw = result;
+			join_email_obj.pw = result;
 			let str = '<div id="email-check-div" class="row d-flex align-items-center mt-2">';
-			str += '<div class="col-7 text-end">인증 메일이 발송되었습니다.</div>';
+			str += '<div class="col-6 text-end">인증 메일이 발송되었습니다.</div>';
+			str += '<div id="email-timer-div" class="col-1 text-end invalid-text-div"></div>';
 			str += '<div class="col-3"><input type="text" id="join-email-pw" class="form-control" placeholder="인증 번호 입력"></div>';
 			str += '<div class="col-2 d-grid"><button type="button" class="btn custom-btn" onclick="email_pw_check(this);">확인</button></div></div>';
 			btn.closest('.row').insertAdjacentHTML('afterend', str);
+			join_email_obj.div = document.querySelector('#email-timer-div');
+			startTimer(join_email_obj);
 		},
 		error: function() {
 			alert('ajax 통신 실패');
@@ -196,22 +229,31 @@ function email_pw_send(btn) {
 
 /* 메일 인증 확인 */
 function email_pw_check(btn) {
+	const email_check_div = document.querySelector('#email-check-div');
+	const email_invalid_div = document.querySelector('#email-invalid-div');
+	if (email_invalid_div != null) {
+		email_invalid_div.remove();
+	}
 	const email_pw = document.querySelector('#join-email-pw');
-	if (email_pw.value == join_email_pw) {
+	if (email_pw.value == join_email_obj.pw) {
+		const email_0_input = document.querySelector('#join-mem-email-0');
+		const email_1_input = document.querySelector('#join-mem-email-1');
 		email_0_input.classList.add('is-valid');
 		email_1_input.classList.add('is-valid');
-		const email_check_div = document.querySelector('#email-check-div');
-		if (email_check_div != null) {
-			email_check_div.remove();
-		}
-		const email_invalid_div = document.querySelector('#email-invalid-div');
-		if (email_invalid_div != null) {
-			email_invalid_div.remove();
-		}
+		email_check_div.remove();
 		return;
 	}
-	
+	let str = '<div id="email-invalid-div" class="row mt-2">';
+	str += '<div class="col-10 text-end invalid-text-div">*인증 번호가 일치하지 않습니다.</div></div>';
+	btn.closest('.row').insertAdjacentHTML('afterend', str);
+	email_check_div.querySelector('input').focus();
 }
+
+/* 전화번호 유효성 검사 */
+
+
+
+
 
 
 
