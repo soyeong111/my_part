75ad13f0c0a3f0f4bb45cf8e4179280a6c836166
yeongseo1204 <dys951204@gmail.com>
2023-05-31 17:13:50 +0900

    ys05311713

--- src/main/resources/static/js/content/member/join.js
+++ src/main/resources/static/js/content/member/join.js
@@ -46,11 +46,7 @@ function id_validation(id_input) {
 		str = '<div class="col-10 offset-2 invalid-text-div">*아이디 확인 버튼을 눌러주세요.</div>';
 	} else {
 		id_input.closest('.row').querySelector('button').disabled = true;
-		if (id_input.value == '') {
-			str = '<div class="col-10 offset-2 invalid-text-div">*아이디는 필수 입력입니다.</div>';
-		} else {
-			str = '<div class="col-10 offset-2 invalid-text-div">*아이디는 3~10자리의, 영문소문자로 시작하는, 영문소문자 혹은 숫자여야합니다.</div>';
-		}
+		str = '<div class="col-10 offset-2 invalid-text-div">*아이디는 3~10자리의, 영문소문자로 시작하는, 영문소문자 혹은 숫자여야합니다.</div>';
 	}
 	id_input.closest('.row').insertAdjacentHTML('beforeend', str);
 }
@@ -105,11 +101,7 @@ function pw_validation(pw_input) {
 		pw_input.classList.remove('is-valid');
 		pw_input.classList.add('is-invalid');
 		pw_check_input.disabled = true;
-		if (pw_input.value == '') {
-			str = '<div class="col-10 offset-2 invalid-text-div">*패스워드는 필수 입력입니다.</div>';
-		} else {
-			str = '<div class="col-10 offset-2 invalid-text-div">*패스워드는 8~16자리의, 영문소문자 + 영문대문자 + 특수문자(! @ # $ % &) + 숫자여야합니다.</div>';
-		}
+		str = '<div class="col-10 offset-2 invalid-text-div">*패스워드는 8~16자리의, 영문소문자 + 영문대문자 + 특수문자(! @ # $ % &) + 숫자여야합니다.</div>';
 	}
 	pw_check_input.classList.remove('is-valid');
 	pw_check_input.classList.add('is-invalid');
@@ -137,29 +129,20 @@ function pw_check(pw_check_input) {
 
 /* 이름 유효성 검사  */
 function name_validation(name_input) {
-	const regexp = /^[가-힣a-zA-Z ]+$/;
-	const invalid_text_div = name_input.closest('.row').querySelector('.invalid-text-div');
-	if (invalid_text_div != null) {
-		invalid_text_div.remove();
-	}
-	let str = '';
-	if (name_input.value.trim() == '') {
-		name_input.value = name_input.value.trim();
-		str = '<div class="col-10 offset-2 invalid-text-div">*이름은 필수 입력입니다.</div>';
-	} else if (regexp.test(name_input.value)) {
-		name_input.classList.remove('is-invalid');
-		name_input.classList.add('is-valid');
-		return;
+	const regexp = /[^가-힣a-zA-Z ]/;
+	name_input.value = name_input.value.replace(regexp, '');
+	name_input.value = name_input.value.trimStart();
+	if (name_input.value == '') {
+		name_input.classList.remove('is-valid');
 	} else {
-		str = '<div class="col-10 offset-2 invalid-text-div">*이름을 확인해주세요.</div>';
+		name_input.classList.add('is-valid');
 	}
-	name_input.classList.remove('is-valid');
-	name_input.classList.add('is-invalid');
-	name_input.closest('.row').insertAdjacentHTML('beforeend', str);
 }
 
 /* 이메일 유효성 검사 */
-function email_validation(email_0_input, email_1_input) {
+function email_validation() {
+	const email_0_input = document.querySelector('#join-mem-email-0');
+	const email_1_input = document.querySelector('#join-mem-email-1');
 	email_0_input.classList.remove('is-valid');
 	email_1_input.classList.remove('is-valid');
 	const email_check_div = document.querySelector('#email-check-div');
@@ -180,23 +163,19 @@ function email_validation(email_0_input, email_1_input) {
 }
 
 /* 이메일 셀렉트 변경 */
-function email_select_change(email_1_input) {
-	if (email_1_input.value == 'self') {
-		const str = '<input type="text" name="memEmailArr" id="join-mem-email-1" class="form-control" onkeyup="email_input_change();" placeholder="직접입력">';
-		email_1_input.parentElement.insertAdjacentHTML('beforeend', str);
-		email_1_input.remove();
+function email_select_change(email_select) {
+	if (email_select.value == 'self') {
+		const str = '<input type="text" name="memEmailArr" id="join-mem-email-1" class="form-control" onkeyup="email_input_change(this);" placeholder="직접입력">';
+		email_select.parentElement.insertAdjacentHTML('beforeend', str);
+		email_select.remove();
 	}
-	const email_0_input = document.querySelector('#join-mem-email-0');
-	email_validation(email_0_input, email_1_input);
+	email_validation();
 }
 
 /* 이메일 인풋 변경 */
-function email_input_change() {
-	const email_0_input = document.querySelector('#join-mem-email-0');
-	const email_1_input = document.querySelector('#join-mem-email-1');
-	email_0_input.value = email_0_input.value.replaceAll(' ', '');
-	email_1_input.value = email_1_input.value.replaceAll(' ', '');
-	email_validation(email_0_input, email_1_input)
+function email_input_change(email_input) {
+	email_input.value = email_input.value.replaceAll(' ', '');
+	email_validation()
 }
 
 /* 메일 인증 클릭 */
@@ -234,8 +213,8 @@ function email_pw_check(btn) {
 	if (email_invalid_div != null) {
 		email_invalid_div.remove();
 	}
-	const email_pw = document.querySelector('#join-email-pw');
-	if (email_pw.value == join_email_obj.pw) {
+	const email_pw_input = document.querySelector('#join-email-pw');
+	if (email_pw_input.value == join_email_obj.pw) {
 		const email_0_input = document.querySelector('#join-mem-email-0');
 		const email_1_input = document.querySelector('#join-mem-email-1');
 		email_0_input.classList.add('is-valid');
@@ -250,22 +229,125 @@ function email_pw_check(btn) {
 }
 
 /* 전화번호 유효성 검사 */
+function tell_validation() {
+	const tell_0_input = document.querySelector('#join-mem-tell-0');
+	const tell_1_input = document.querySelector('#join-mem-tell-1');
+	const tell_2_input = document.querySelector('#join-mem-tell-2');
+	tell_0_input.classList.remove('is-valid');
+	tell_1_input.classList.remove('is-valid');
+	tell_2_input.classList.remove('is-valid');
+	const tell_check_div = document.querySelector('#tell-check-div');
+	if (tell_check_div != null) {
+		tell_check_div.remove();
+	}
+	const tell_invalid_div = document.querySelector('#tell-invalid-div');
+	if (tell_invalid_div != null) {
+		tell_invalid_div.remove();
+	}
+	const regexp1 = /^[0-9]{3,4}$/;
+	const regexp2 = /^[0-9]{4}$/;
+	if (regexp1.test(tell_1_input.value) && regexp2.test(tell_2_input.value)) {
+		tell_0_input.closest('.row').querySelector('button').disabled = false;
+	} else {
+		tell_0_input.closest('.row').querySelector('button').disabled = true;
+	}
+}
 
+/* 전화번호 인풋 변경 */
+function tell_change(tell_input) {
+	tell_input.value = tell_input.value.replace(/\D/, '');
+	tell_validation();
+}
 
+/* 전화번호 인증 클릭 */
+function tell_pw_send(btn) {
+	const tell_0_input = document.querySelector('#join-mem-tell-0');
+	const tell_1_input = document.querySelector('#join-mem-tell-1');
+	const tell_2_input = document.querySelector('#join-mem-tell-2');
+	btn.disabled = true;
+	const tell = tell_0_input.value + tell_1_input.value + tell_2_input.value;
+	console.log(tell);
+	$.ajax({
+		url: '/member/tellAuthAjax',
+		type: 'post',
+		contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+		data: {'to':tell},
+		success: function(result) {
+			console.log(result);
+			join_tell_obj.pw = result;
+			let str = '<div id="tell-check-div" class="row d-flex align-items-center mt-2">';
+			str += '<div class="col-6 text-end">인증 문자가 발송되었습니다.</div>';
+			str += '<div id="tell-timer-div" class="col-1 text-end invalid-text-div"></div>';
+			str += '<div class="col-3"><input type="text" id="join-tell-pw" class="form-control" placeholder="인증 번호 입력"></div>';
+			str += '<div class="col-2 d-grid"><button type="button" class="btn custom-btn" onclick="tell_pw_check(this);">확인</button></div></div>';
+			btn.closest('.row').insertAdjacentHTML('afterend', str);
+			join_tell_obj.div = document.querySelector('#tell-timer-div');
+			startTimer(join_tell_obj);
+		},
+		error: function() {
+			alert('ajax 통신 실패');
+		}
+	});
+}
 
-
-
-
-
+/* 전화번호 인증 확인 */
+function tell_pw_check(btn) {
+	const tell_check_div = document.querySelector('#tell-check-div');
+	const tell_invalid_div = document.querySelector('#tell-invalid-div');
+	if (tell_invalid_div != null) {
+		tell_invalid_div.remove();
+	}
+	const tell_pw_input = document.querySelector('#join-tell-pw');
+	if (tell_pw_input.value == join_tell_obj.pw) {
+		const tell_0_input = document.querySelector('#join-mem-tell-0');
+		const tell_1_input = document.querySelector('#join-mem-tell-1');
+		const tell_2_input = document.querySelector('#join-mem-tell-2');
+		tell_0_input.classList.add('is-valid');
+		tell_1_input.classList.add('is-valid');
+		tell_2_input.classList.add('is-valid');
+		tell_check_div.remove();
+		return;
+	}
+	let str = '<div id="tell-invalid-div" class="row mt-2">';
+	str += '<div class="col-10 text-end invalid-text-div">*인증 번호가 일치하지 않습니다.</div></div>';
+	btn.closest('.row').insertAdjacentHTML('afterend', str);
+	tell_check_div.querySelector('input').focus();
+}
 
 /* 주소록 api 사용 */
 function search_addr() {
+	const addr_input = document.querySelector('#join-mem-addr');
 	new daum.Postcode({
 		oncomplete: function(data) {
 			const road_addr = data.roadAddress;
-			document.querySelector('#join-form #join-mem-addr').value = road_addr;
+			addr_input.value = road_addr;
+		},
+		onclose: function(state) {
+			if (addr_input.value != '') {
+				addr_input.classList.add('is-valid');
+			}
 		}
 	}).open();
+}
+
+/* 상세주소 유효성 검사  */
+function addr_validation(addr_input) {
+	addr_input.value = addr_input.value.trimStart();
+	if (addr_input.value == '') {
+		addr_input.classList.remove('is-valid');
+	} else {
+		addr_input.classList.add('is-valid');
+	}
+}
+
+/* 필수 사항 체크 */
+function require_check() {
+	const tags = document.querySelectorAll('.is-valid');
+	if (tags.length == 11) {
+		join();
+	} else {
+		alert('생년월일과 성별을 제외한\n모든 정보는 필수 입력 사항입니다.');
+	}
 }
 
 /* 회원가입 */
