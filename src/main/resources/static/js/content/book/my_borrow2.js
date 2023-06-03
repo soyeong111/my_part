c755251a1c7cb621ebcddea7ac1f9e9864d829d0
huhjs <xxxyzzzky@gamil.com>
2023-06-03 23:30:18 +0900

    하기시러

--- src/main/resources/static/js/content/book/my_borrow2.js
+++ src/main/resources/static/js/content/book/my_borrow2.js
@@ -7,7 +7,7 @@ function extend(btn, name, borrowCode, bookCode) {
       icon: 'question',
       showCancelButton: true,
       confirmButtonText: '네',
-      cancelButtonText: '아니오',
+      cancelButtonText: '아니요',
     }).then((result) => {
       if (result.isConfirmed) {
         $.ajax({
@@ -17,7 +17,11 @@ function extend(btn, name, borrowCode, bookCode) {
           contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
           data: { 'borrowCode': borrowCode, 'memId': name },
           success: function(result) {
-	          },
+			   Swal.fire('도서 반납 기한이 \n3일 연장되었습니다.', '', 'success')
+              .then(() => {
+                location.reload();
+              });
+          },
           error: function() {
             Swal.fire('연장에 실패했습니다.', '', 'error');
           }
@@ -34,13 +38,34 @@ function extend(btn, name, borrowCode, bookCode) {
 function returnBook(btn, name, borrowCode, bookCode) {
   var button = document.getElementById('reBtn-' + borrowCode);
   if (button && button.value === '반납') {
-    button.style.display = 'none'; // 버튼 숨기기
-    var span = document.createElement('span'); // 텍스트 엘리먼트 생성
-    span.textContent = '완료'; // 텍스트 설정
-    button.parentNode.appendChild(span); // 텍스트 추가
+    Swal.fire({
+      title: '해당 도서를 반납하시겠습니까?',
+      icon: 'question',
+      showCancelButton: true,
+      confirmButtonText: '네',
+      cancelButtonText: '아니요',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        $.ajax({
+          url: '/book/returnBookAjax',
+          type: 'post',
+          async: true,
+          contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+          data: { 'borrowCode': borrowCode, 'memId': name },
+          success: function(result) {
+			   Swal.fire('도서가 성공적으로 \n반납되었습니다.', '', 'success')
+              .then(() => {
+                location.reload();
+              });
+          },
+          error: function() {
+            Swal.fire('연장에 실패했습니다.', '', 'error');
+          }
+        });
+      }
+    });
   }
 }
-
 
 
 
