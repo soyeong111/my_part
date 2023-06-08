c83929127468bb2034613debf44f025619e94bbf
yeongseo1204 <dys951204@gmail.com>
2023-06-08 18:12:00 +0900

    ys06081811

---
+++ src/main/resources/static/js/fragment/member_side.js
@@ -0,0 +1,20 @@
+
+/* side-main-menu-div에 마우스 올라갈 때 */
+function main_menu_mouseenter(main_menu_div) {
+	console.log('in');
+	const collapse_div = main_menu_div.querySelector('.collapse');
+	if (collapse_div != null) {
+		const collapse = bootstrap.Collapse.getOrCreateInstance(collapse_div);
+		collapse.show();
+	}
+}
+
+/* side-main-menu-div에서 마우스 내려올 때 */
+function main_menu_mouseleave(main_menu_div) {
+	console.log('out');
+	const collapse_div = main_menu_div.querySelector('.collapse');
+	if (collapse_div != null) {
+		const collapse = bootstrap.Collapse.getOrCreateInstance(collapse_div);
+		collapse.hide();
+	}
+}
