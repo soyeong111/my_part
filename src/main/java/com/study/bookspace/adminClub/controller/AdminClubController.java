d0ad2fbdf2cdb273e1261f17e9f129a414301bd0
sue0336 <suesue0336@gmail.com>
2023-06-12 09:47:18 +0900

    zzz

---
+++ src/main/java/com/study/bookspace/adminClub/controller/AdminClubController.java
@@ -0,0 +1,11 @@
+package com.study.bookspace.adminClub.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/aClub")
+public class AdminClubController {
+	
+	
+}
