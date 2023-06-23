20ff0592c64ed7ea5df0b1ac5b1a4f002f0ca4d5
huhjs <xxxyzzzky@gamil.com>
2023-06-23 20:04:10 +0900

    Zz

--- src/main/resources/static/js/content/book/reg_book.js
+++ src/main/resources/static/js/content/book/reg_book.js
@@ -1,68 +1,84 @@
 function regBook() {
+	
+	alert(111);
   // 입력값 가져오기
-  const bookTitle = document.getElementById("#bookTitle").value;
-  const bookAuthor = document.getElementById("#bookAuthor").value;
-  const bookPublisher = document.getElementById("#bookPublisher").value;
-  const isbn = document.getElementById("#isbn").value;
-  const bookPublicationDate = document.getElementById("#bookPublicationDate").value;
-  const mainImgFile = document.getElementById("#mainImg").files[0];
-  const subImgFile = document.getElementById("#subImg").files[0];
-  const bookIntro = document.getElementById("#bookIntro").value;
+  const bookTitle = document.querySelector("#bookTitle");
+  bookTitle.value = bookTitle.value.trim();
+  
+  const bookAuthor = document.querySelector("#bookAuthor");
+  bookAuthor.value = bookAuthor.value.trim();
+  
+  const bookPublisher = document.querySelector("#bookPublisher");
+  bookPublisher.value = bookPublisher.value.trim();
+  
+  const isbn = document.querySelector("#isbn");
+  isbn.value = isbn.value.trim();
+  
+  
+  
+  const mainImgFile = document.querySelector("#mainImg").files[0];
+  const subImgFile = document.querySelector("#subImg").files[0];
+  
+  const bookIntro = document.querySelector("#bookIntro");
+  bookIntro.value = bookIntro.value.trim();
 
 
 
 
 
   // 입력값 유효성 검사
-  if (bookTitle.trim() === "") {
+  if (bookTitle.value === "") {
     // 제목이 비어있는 경우
-    alert("제목을 입력해주세요.");
+     Swal.fire("도서명은 필수 입력입니다.");
     return;
   }
 
-  if (bookAuthor.trim() === "") {
+  if (bookAuthor.value === "") {
     // 작가가 비어있는 경우
-    alert("작가를 입력해주세요.");
+   Swal.fire("작가명은 필수입니다.");
     return;
   }
 
-  if (bookPublisher.trim() === "") {
+  if (bookPublisher.value === "") {
     // 출판사가 비어있는 경우
-    alert("출판사를 입력해주세요.");
+     Swal.fire("출판사는 필수입력입니다.");
     return;
   }
 
-  if (isbn.trim() === "") {
+  if (!/^\d+$/.test(isbn.value)) {
     // ISBN이 비어있는 경우
-    alert("ISBN을 입력해주세요.");
+    Swal.fire("ISBN은 숫자만 입력 가능합니다.");
     return;
   }
 
-  if (bookPublicationDate.trim() === "") {
-    // 발행일이 비어있는 경우
-    alert("발행일을 입력해주세요.");
-    return;
+
+  if (mainImgFile) {
+    var mainImgFileType = mainImgFile.type;
+    if (mainImgFileType !== "image/jpeg") {
+      Swal.fire("앞 이미지는 JPG 형식만 가능합니다.");
+      return false;
+    }
   }
 
-  if (mainImgFile === undefined) {
-    // 앞 이미지가 선택되지 않은 경우
-    alert("앞 이미지를 선택해주세요.");
-    return;
+  if (subImgFile) {
+    var subImgFileType = subImgFile.type;
+    if (subImgFileType !== "image/jpeg") {
+      Swal.fire("뒤 이미지는 JPG 형식만 가능합니다.");
+      return false;
+    }
   }
-
-  if (subImgFile === undefined) {
-    // 뒤 이미지가 선택되지 않은 경우
-    alert("뒤 이미지를 선택해주세요.");
-    return;
-  }
-
-  if (bookIntro.trim() === "") {
+  
+  
+  if (bookIntro.value === "") {
     // 도서 소개가 비어있는 경우
-    alert("도서 소개를 작성해주세요.");
+    Swal.fire("도서 소개는 필수 입력값입니다.");
     return;
   }
 
   const regBook_form = document.querySelector('#regBookForm');
 		regBook_form.submit();
-
+		
+		  // 등록 성공 알림창 표시
+ 
 }
+
