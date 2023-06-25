42852bbd4f98265b998e4fdd3195f00dd3dc084b
yeongseo1204 <dys951204@gmail.com>
2023-06-26 00:13:07 +0900

    ys06260013

---
+++ src/main/resources/static/js/content/my/withdrawal.js
@@ -0,0 +1,52 @@
+
+/* 비밀번호 입력 시 */
+function mem_pw_keyup(mem_pw_input) {
+	mem_pw_input.value = mem_pw_input.value.replaceAll(' ', '');
+}
+
+/* 비밀번호 확인 */
+function check_pw() {
+	const mem_pw_input = document.querySelector('#mem-pw');
+	$.ajax({
+		url: '/mMember/checkPwAjax',
+		type: 'post',
+		contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+		data: {'memPw':mem_pw_input.value},
+		success: function(result) {
+			if (result) {
+				if (confirm('정말 탈퇴 하시겠습니까?')) {
+					withdrawal();
+				} else {
+					location.reload();
+				}
+			} else {
+				mem_pw_input.classList.add('is-invalid');
+				mem_pw_input.focus();
+			}
+		},
+		error: function() {
+			alert('ajax 통신 실패');
+		}
+	});
+}
+
+// 회원 탈퇴
+function withdrawal() {
+	$.ajax({
+		url: '/mMember/withdrawalAjax',
+		type: 'post',
+		contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+		data: {},
+		success: function(result) {
+			if (result) {
+				alert('탈퇴 되었습니다.');
+				location.href = '/member/logout';
+			} else {
+				alert('탈퇴 실패');
+			}
+		},
+		error: function() {
+			alert('ajax 통신 실패');
+		}
+	});
+}
