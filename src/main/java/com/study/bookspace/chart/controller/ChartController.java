b49ec51351992b8bc724d242e8f854aaafd00df1
yeongseo1204 <dys951204@gmail.com>
2023-06-28 17:20:25 +0900

    ys06281720

--- src/main/java/com/study/bookspace/chart/controller/ChartController.java
+++ src/main/java/com/study/bookspace/chart/controller/ChartController.java
@@ -1,8 +1,13 @@
 package com.study.bookspace.chart.controller;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
 
 @Controller
 @RequestMapping("/chart")
@@ -11,6 +16,17 @@ public class ChartController {
 	@GetMapping("/pythonChart")
 	public String pythonChart() {
 		return "content/chart/python_chart";
+	}
+	
+	@ResponseBody
+	@PostMapping("/getChartDataAjax")
+	public Map<String, Object> getChartDataAjax() {
+		System.out.println("!!!!!!!!!");
+		RestTemplate restTemplate = new RestTemplate();
+		String url = "http://localhost:5000/getLibraryData";
+		Map<String, Object> chartData = restTemplate.getForObject(url, Map.class);
+		System.out.println(chartData);
+		return chartData;
 	}
 	
 }
