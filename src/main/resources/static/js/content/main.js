9fb3ae9c234813b4b254655c32415f5441ef0827
yeongseo1204 <dys951204@gmail.com>
2023-07-04 08:52:25 +0900

    ys07040852

--- src/main/resources/static/js/content/main.js
+++ src/main/resources/static/js/content/main.js
@@ -3,10 +3,10 @@
     var calendarEl = document.getElementById('calendar');
 
     var calendar = new FullCalendar.Calendar(calendarEl, {
-    	googleCalendarApiKey : "AIzaSyAWw1w8auy0TJQOTl-YHfi8R9CFnL9WegA",
+    	googleCalendarApiKey : '',
         eventSources :[ 
             {
-                googleCalendarId : '69d9f3562fdd3d1d5f6df7da5a903719b34dad458924f4d15dac7b5792319b01@group.calendar.google.com'
+                googleCalendarId : ''
                 , color: '#4e68b9'   // an option!
                 , textColor: 'white' // an option!
             } 
